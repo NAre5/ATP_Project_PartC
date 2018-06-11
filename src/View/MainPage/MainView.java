2bcc3fe20e0d8f557ac3e1603e91d71a8b629812
erant <sherlord5@gmail.com>
2018-06-11 17:03:02 +0300

    Initial commit

---
+++ src/View/MainPage/MainView.java
@@ -0,0 +1,15 @@
+package View.MainPage;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainView implements Initializable{
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
