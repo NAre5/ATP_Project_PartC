b000db62f295a5d66065704664f6e5809f441d81
michaelmichaelshvili <michaelm748@gmail.com>
2018-06-11 23:21:00 +0300

    New

--- src/View/NEW/NewViewModel.java
+++ src/View/NEW/NewViewModel.java
@@ -1,13 +1,38 @@
 package View.NEW;
 
+import View.AView;
+import View.MainPage.MainView;
+import algorithms.mazeGenerators.Maze;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-/**
- * Created by Michael Michaelshvili on 06/06/2018.
- */
-public class NewViewModel {
+
+public class NewViewModel extends AView {
     public Button next;
-    public NumberTextField text_rowNum;
-    public NumberTextField text_columnNum;
+    public TextField text_rowNum;
+    public TextField text_columnNum;
 
+    public void StartGame(ActionEvent actionEvent)
+    {
+        int row;
+        int column;
+        try
+        {
+            row = Integer.parseInt(text_rowNum.getText());
+            column = Integer.parseInt(text_columnNum.getText());
+        }catch (NumberFormatException e)
+        {
+            showAlert("Enter only number");
+            return;
+        }
+        catch (NullPointerException e)
+        {
+            showAlert("Enter only number");
+            return;
+        }
+        MainView m = new MainView();
+        m.displayMaze(viewModel.generateMaze(row,column));
+    }
+    public onKey
 }
